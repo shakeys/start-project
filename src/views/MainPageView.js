2dc889c10b66f3e062c868a423b6f786963e1015
Vince <veridianoaris@gmail.com>
2020-10-15 01:40:24 +0800

    Update Mail List UI>

--- src/views/MainPageView.js
+++ src/views/MainPageView.js
@@ -1,7 +1,8 @@
 import React from "react";
 import clsx from "clsx";
-import { withStyles, Box, makeStyles, Grid, Typography } from "@material-ui/core";
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import { withStyles, Box, makeStyles, Grid, Typography, Button } from "@material-ui/core";
+import FlagIcon from '@material-ui/icons/Flag';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import MainLayout from "../layout/MainLayout";
 
@@ -12,7 +13,7 @@ const MailList = withStyles((_theme) => ({
     position: "fixed",
     width: 500,
     height: "calc(100% - 90px)",
-    overflowY: "scroll",
+    overflowY: "auto",
   },
 }))(Box);
 
@@ -20,7 +21,7 @@ const useStyles = makeStyles((_theme) => ({
   mail: {
     borderBottom: `1px solid ${_theme.palette.grey[300]}`,
     padding: "20px",
-    color: _theme.palette.grey[700],
+    color: _theme.palette.grey[500],
     '&:hover': {
       cursor: "pointer",
       backgroundColor: "#b3e5fc",
@@ -55,6 +56,25 @@ const useStyles = makeStyles((_theme) => ({
     overflow: "hidden",
     textOverflow: "ellipsis",
   },
+  tags: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  iconBtn: {
+    minWidth: "0",
+    padding: "0px",
+    marginLeft: 2,
+  },
+  iconSize: {
+    width: 20,
+    height: 20,
+  },
+  trashBtn: {
+    color: _theme.palette.error.main,
+  },
+  flagBtn: {
+    color: _theme.palette.success.main,
+  },
 }));
 
 function MainPageView() {
@@ -71,8 +91,12 @@ function MainPageView() {
                 <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
               </Box>
 
-              <Box>
+              <Box className={classes.mailerHeader}>
                 <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
+                <Box>
+                  <Button type="text" className={clsx(classes.iconBtn, classes.flagBtn)}><FlagIcon className={classes.iconSize} /></Button>
+                  <Button type="text" className={clsx(classes.iconBtn, classes.trashBtn)}><DeleteIcon className={classes.iconSize} /></Button>
+                </Box>
               </Box>
 
               <Box pt={1} component="div" className={classes.subject}>
@@ -82,201 +106,18 @@ function MainPageView() {
           </Grid>
 
           <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
+            <Box className={clsx(classes.mail)}>
               <Box className={classes.mailerHeader}>
                 <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
                 <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
               </Box>
 
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
               <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
                 <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
-              <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
-              <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
-              <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
-              <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
-              <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
-              <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
-              <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
-              <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
-              <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
-              </Box>
-
-              <Box pt={1} component="div" className={classes.subject}>
-                <Typography variant="body1" component="label">Planning your first sprint in Rebar Sprints Planning your first sprint in Rebar Sprints</Typography>
-              </Box>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Box className={clsx(classes.mail, classes.unread)}>
-              <Box className={classes.mailerHeader}>
-                <Typography variant="h5" component="p" className={classes.mailerName}>Ronnel Dela Cruz</Typography>
-                <Typography variant="subtitle2" component="p" className={classes.mailerTime}>03:00 PM</Typography>
-              </Box>
-
-              <Box>
-                <Typography variant="subtitle2" component="p">ronnel@rebar.ph</Typography>
+                <Box>
+                  <Button type="text" className={clsx(classes.iconBtn, classes.flagBtn)}><FlagIcon className={classes.iconSize} /></Button>
+                  <Button type="text" className={clsx(classes.iconBtn, classes.trashBtn)}><DeleteIcon className={classes.iconSize} /></Button>
+                </Box>
               </Box>
 
               <Box pt={1} component="div" className={classes.subject}>
