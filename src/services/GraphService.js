e49ac830da3fa496407cc25f398af0eb81600e90
Michael Yambao <michael@rebar.ph>
2020-10-14 23:49:48 +0800

    getUserEvents function

--- src/services/GraphService.js
+++ src/services/GraphService.js
@@ -1,64 +1,72 @@
-﻿var graph = require('@microsoft/microsoft-graph-client');
+﻿var graph = require("@microsoft/microsoft-graph-client");
 
 function getAuthenticatedClient(accessToken) {
-    // Initialize Graph client
-    const client = graph.Client.init({
-        // Use the provided access token to authenticate
-        // requests
-        authProvider: (done) => {
-            done(null, accessToken.accessToken);
-        }
-    });
+  // Initialize Graph client
+  const client = graph.Client.init({
+    // Use the provided access token to authenticate
+    // requests
+    authProvider: (done) => {
+      done(null, accessToken.accessToken);
+    },
+  });
 
-    return client;
+  return client;
 }
 
 export async function getUserDetails(userId, accessToken) {
+  const client = getAuthenticatedClient(accessToken);
 
-    const client = getAuthenticatedClient(accessToken);
+  const user = await client.api("/" + userId).get();
 
-    const user = await client.api('/' + userId).get();
-
-    return user;
+  return user;
 }
 
 export async function getUserPhoto(userId, accessToken) {
+  var url = "https://graph.microsoft.com/v1.0/" + userId + "/photo/$value";
 
-    var url = 'https://graph.microsoft.com/v1.0/' + userId + '/photo/$value';
-
-    return fetch(url, {
-        method: 'GET',
-        headers: {
-            Authorization: 'Bearer ' + accessToken.accessToken,
-            'Content-Type': 'application/json',
-        },
-    });
-
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + accessToken.accessToken,
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 export async function getUserEmail(userId, accessToken) {
+  const client = graph.Client.init({
+    authProvider: (done) => {
+      done(null, accessToken.accessToken);
+    },
+  });
 
-    const client = graph.Client.init({
-        authProvider: (done) => {
-            done(null, accessToken.accessToken);
-        }
-    });
+  let res = await client
+    .api("/users/25d251d7-f5c3-4f60-b9aa-72f079d32ba1/messages")
+    .select("sender,subject")
+    .get();
+}
 
-    let res = await client.api('/users/25d251d7-f5c3-4f60-b9aa-72f079d32ba1/messages')
-	.select('sender,subject')
-	.get();
+export async function getUserEvents(userId, accessToken) {
+  const client = graph.Client.init({
+    authProvider: (done) => {
+      done(null, accessToken.accessToken);
+    },
+  });
 
+  let res = await client
+    .api("/users/25d251d7-f5c3-4f60-b9aa-72f079d32ba1/events")
+    .select("subject,body,bodyPreview,organizer,attendees,start,end,location")
+    .get();
 }
 
 export async function getAllUsers(accessToken) {
-    var url = 'https://graph.microsoft.com/v1.0/users'
+  var url = "https://graph.microsoft.com/v1.0/users";
 
-    return fetch(url, {
-        method: 'GET',
-        headers: {
-            Authorization: 'Bearer ' + accessToken.accessToken,
-            'Content-Type': 'application/json',
-        },
-    });
-
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + accessToken.accessToken,
+      "Content-Type": "application/json",
+    },
+  });
 }
